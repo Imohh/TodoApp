6ccd743fe259858148d0029360adde040dafa10b
Imoh Precious <jaypee88830@gmail.com>
2022-03-01 02:07:00 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { useContext } from 'react'
 import TodoForm from './components/TodoForm'
 // import Toggle from './components/Toggle'
 import './components/style.css'
-import { ThemeContext } from './context/context'
 
 const App = () => {
-    const theme = useContext(ThemeContext)
-    const darkMode = theme.state.darkMode
 
     return (
         <>
